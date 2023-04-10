996939ecad9fc61d110d54460bafbd2ce034d941
elleven11 <federico.cassano@federico.codes>
2023-04-10 15:04:07 -0400

    training scripts

--- run_type_annotation_fim.sh
+++ run_type_annotation_fim.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters"
+    echo "Usage: ./run_type_annotation_fim.sh <num_gpus> <batch_size> <num_workers>"
+    exit 1
+fi
+
 python -m torch.distributed.launch \
-        --nproc_per_node 3 train.py \
+        --nproc_per_node $1 train.py \
         --model_path="bigcode/santacoder" \
         --dataset_name="nuprl/ts-training" \
         --output_dir="./chk2" \
@@ -9,7 +15,7 @@ python -m torch.distributed.launch \
         --split="train" \
         --seq_length 2048 \
         --max_steps 30000 \
-        --batch_size 2 \
+        --batch_size $2 \
         --gradient_accumulation_steps 8 \
         --learning_rate 5e-5 \
         --num_warmup_steps 100 \
@@ -19,6 +25,9 @@ python -m torch.distributed.launch \
         --fim_spm_rate 0.5 \
         --streaming \
         --log_freq 1 \
-        --num_workers="20" \
+        --num_workers="$3" \
         --no_fp16 \
-        --bf16
+        --bf16 \
+        --hub_model_id="gammatau/santacoder-ts-fim" \
+        --push_to_hub True \
+
