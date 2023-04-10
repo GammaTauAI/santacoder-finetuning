cbf89ee8a122f06b2093c0d2fc5c638eea60c4d0
elleven11 <federico.cassano@federico.codes>
2023-04-10 15:35:12 -0400

    fix fim for unicode

--- fim.py
+++ fim.py
@@ -12,11 +12,14 @@ Language.build_library(
     f"{Path(__file__).parent}/build/languages.so",
     [f"{Path(__file__).parent}/tree-sitter-typescript/typescript"]
 )
-TS_LANGUAGE = Language(f"{Path(__file__).parent}/build/languages.so", 'typescript')
+TS_LANGUAGE = Language(
+    f"{Path(__file__).parent}/build/languages.so", 'typescript')
 PARSER = Parser()
 PARSER.set_language(TS_LANGUAGE)
 
 # this is expensive so we cache it
+
+
 @functools.lru_cache(maxsize=None)
 def get_fim_token_ids(tokenizer):
     try:
@@ -30,11 +33,14 @@ def get_fim_token_ids(tokenizer):
         suffix_tok_id, prefix_tok_id, middle_tok_id, pad_tok_id = None, None, None, None
     return suffix_tok_id, prefix_tok_id, middle_tok_id, pad_tok_id
 
+
 def build(content):
     def create_query(query):
         return TS_LANGUAGE.query(query)
+
     def str_to_tree(contents):
         return PARSER.parse(bytes(contents, "utf-8"))
+
     def is_child_type_annotation(node):
         """Checks if any of the parent nodes is an annotation node."""
         node = node.parent
@@ -82,11 +88,8 @@ def build(content):
 
     return new_content
 
-def get_prefix_middle_suffix(np_rng: RandomState, sample: str) -> Optional[Tuple[Tuple[str, str, str], RandomState]]:
-    def create_query(query):
-        return TS_LANGUAGE.query(query)
-    def str_to_tree(contents):
-        return PARSER.parse(bytes(contents, "utf-8"))
+
+def get_prefix_middle_suffix(np_rng: RandomState, sample: bytes) -> Optional[Tuple[Tuple[bytes, bytes, bytes], RandomState]]:
     def is_child_type_annotation(node):
         """Checks if any of the parent nodes is an annotation node."""
         node = node.parent
@@ -96,14 +99,14 @@ def get_prefix_middle_suffix(np_rng: RandomState, sample: str) -> Optional[Tuple
             node = node.parent
         return False
 
-    QUERY = create_query("""
+    QUERY = TS_LANGUAGE.query("""
 [
   (type_annotation) @annotation
   (opting_type_annotation) @annotation
   (omitting_type_annotation) @annotation
 ]
 """)
-    tree = str_to_tree(sample)
+    tree = PARSER.parse(sample)
 
     # Each capture has a start_byte and end_byte; these are the indices of the
     # type annotation. We want to invert these indices, i.e. get the substrings
@@ -120,21 +123,23 @@ def get_prefix_middle_suffix(np_rng: RandomState, sample: str) -> Optional[Tuple
         return None
     random_pick_i = np_rng.choice(captures_no_child)
 
-    prefix_str: str = sample[:captures[random_pick_i][0].start_byte]
-    middle_str: str = sample[captures[random_pick_i][0].start_byte:captures[random_pick_i][0].end_byte]
-    if middle_str.startswith(": "):
-        prefix_str += ": "
+    prefix_str: bytes = sample[:captures[random_pick_i][0].start_byte]
+    middle_str: bytes = sample[captures[random_pick_i]
+                               [0].start_byte:captures[random_pick_i][0].end_byte]
+    if middle_str.startswith(b": "):
+        prefix_str += b": "
         middle_str = middle_str[2:]
-    suffix_str: str = ""
+    suffix_str: bytes = b""
     l = len(captures)
     for i in range(random_pick_i, l - 1):
-        suffix_str += sample[captures[i][0].end_byte:captures[i + 1][0].start_byte]
+        suffix_str += sample[captures[i]
+                             [0].end_byte:captures[i + 1][0].start_byte]
     suffix_str += sample[captures[l - 1][0].end_byte:]
 
     return (prefix_str, middle_str, suffix_str), np_rng
 
 
-## Adapted from https://github.com/bigcode-project/Megatron-LM/blob/6c4bf908df8fd86b4977f54bf5b8bd4b521003d1/megatron/data/gpt_dataset.py
+# Adapted from https://github.com/bigcode-project/Megatron-LM/blob/6c4bf908df8fd86b4977f54bf5b8bd4b521003d1/megatron/data/gpt_dataset.py
 def permute(
     tokenizer,
     sample,
@@ -151,16 +156,15 @@ def permute(
     """
 
     if np_rng.binomial(1, fim_rate):
-        boundaries = list(np_rng.randint(low=0, high=len(sample) + 1, size=2))
-        boundaries.sort()
+        decoded_bytes: bytes = tokenizer.decode(sample)
+        assert isinstance(decoded_bytes, bytes)  # just making sure
 
-        # prefix = np.array(sample[: boundaries[0]], dtype=np.int64)
-        # middle = np.array(sample[boundaries[0] : boundaries[1]], dtype=np.int64)
-        # suffix = np.array(sample[boundaries[1] :], dtype=np.int64)
-        res = get_prefix_middle_suffix(np_rng, tokenizer.decode(sample))
+        res = get_prefix_middle_suffix(np_rng, decoded_bytes)
         if res is None:
             return None, np_rng
+
         (prefix_str, middle_str, suffix_str), np_rng = res
+
         prefix = np.array(tokenizer.encode(prefix_str))
         middle = np.array(tokenizer.encode(middle_str))
         suffix = np.array(tokenizer.encode(suffix_str))
@@ -193,3 +197,31 @@ def permute(
         new_sample = sample
 
     return list(new_sample), np_rng
+
+
+if __name__ == "__main__":  # some unit tests
+    import os
+    rng = np.random.RandomState(seed=int(os.urandom(4).hex(), 16))
+    sample = """
+    function foo(x: number, y: number): number {
+        return x + y;
+    }
+
+    // some unicode to mess things up
+    // 😀 😃 😄 😁 😆 😅
+
+    function foo2(x: number, y: number): number {
+        return x + y;
+    }
+    """
+    bytes_sample = bytes(sample, "utf-8")
+    print("sample:", sample)
+    print("bytes_sample:", bytes_sample)
+
+    print("get_prefix_middle_suffix:")
+    res = get_prefix_middle_suffix(rng, bytes_sample)
+    if res is not None:
+        (prefix_str, middle_str, suffix_str), rng = res
+        print("prefix_str:", prefix_str.decode("utf-8"))
+        print("middle_str:", middle_str.decode("utf-8"))
+        print("suffix_str:", suffix_str.decode("utf-8"))
